dfb1a13be9e08f64dfe7cdaf792fdd22e397d4fa
guodont <dream0147er@gmail.com>
2015-06-30 15:16:58 +0000

    add Users Model and test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # 根据所在的页面返回完整的标题
+  
+  def full_title(page_title = '')
+    base_title = "毕业生档案管理系统"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_PHONE_REGEX = /\A^[1][358][0-9]{9}$/
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  #validates :phone, presence: false, length: { maximum: 11 },
+  #                  format: { with: VALID_PHONE_REGEX }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  
+end
