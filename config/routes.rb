fa91a9dd41d44300e274ab3ef6c1b3dcafac05d9
guodont <dream0147er@gmail.com>
2015-07-02 01:31:11 +0000

    ooo

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,6 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-  
-  #get 'static_pages/home'
-
   get 'help' => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'signup' =>  'users#new'
